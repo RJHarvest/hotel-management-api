a01e65a9c4f8e82ffaa72b9c8361a866c5e7191b
Russell Gapasin <russell@fanswifi.com>
2020-04-28 10:51:51 +0800

    Added middlewares

---
+++ middlewares/reqAuth.js
@@ -0,0 +1,30 @@
+const reqStaffAuth = (req, res, next) => {
+  const isStaffLoggedIn = req.user && req.user.staff
+  if (!isStaffLoggedIn) {
+    return res.status(409).send({ error: 'You must be logged in!' })
+  }
+  next()
+}
+
+const reqGuestAuth = (req, res, next) => {
+  const isGuestLoggedIn = req.user && req.user.guest
+  if (!isGuestLoggedIn) {
+    return res.status(409).send({ error: 'You must be logged in!' })
+  }
+  next()
+}
+
+const reqAuth = (req, res, next) => {
+  const isGuestLoggedIn = req.user && req.user.guest
+  const isStaffLoggedIn = req.user && req.user.staff
+  if (!isGuestLoggedIn || !isStaffLoggedIn) {
+    return res.status(409).send({ error: 'You must be logged in!' })
+  }
+  next()
+}
+
+module.exports = {
+  reqStaffAuth,
+  reqGuestAuth,
+  reqAuth,
+}
---
+++ middlewares/reqPermission.js
@@ -0,0 +1,42 @@
+const reqFrontDeskPermission = (req, res, next) => {
+  const isStaffUser = req.user && req.user.staff
+  const isFrontDeskRole = isStaffUser && req.user.staff.role === 'front_desk'
+  if (!isFrontDeskRole) {
+    return res.status(409).send({ error: 'You do not have permission!' })
+  }
+  next()
+}
+
+const reqRoomServicePermission = (req, res, next) => {
+  const isStaffUser = req.user && req.user.staff
+  const isRoomServiceRole = isStaffUser && req.user.staff.role === 'room_service'
+  if (!isRoomServiceRole) {
+    return res.status(409).send({ error: 'You do not have permission!' })
+  }
+  next()
+}
+
+const reqKitchenPermission = (req, res, next) => {
+  const isStaffUser = req.user && req.user.staff
+  const isKitchenRole = isStaffUser && req.user.staff.role === 'kitchen'
+  if (!isKitchenRole) {
+    return res.status(409).send({ error: 'You do not have permission!' })
+  }
+  next()
+}
+
+const reqManagerPermission = (req, res, next) => {
+  const isStaffUser = req.user && req.user.staff
+  const isManagerRole = isStaffUser && req.user.staff.role === 'manager'
+  if (!isManagerRole) {
+    return res.status(409).send({ error: 'You do not have permission!' })
+  }
+  next()
+}
+
+module.exports = {
+  reqFrontDeskPermission,
+  reqRoomServicePermission,
+  reqKitchenPermission,
+  reqManagerPermission,
+}
