6c3df9134ba5219334917cc81f275c949824985d
Russell Gapasin <russell@fanswifi.com>
2020-05-12 02:35:47 +0800

    Added findAllAccessible methods for every table

--- models/index.js
+++ models/index.js
@@ -26,22 +26,38 @@ class ModelManager {
     this.Category.belongsTo(this.Menu, { foreignKey: 'category_id' })
     this.Menu.hasMany(this.Category, { foreignKey: 'category_id' })
     
-    this.Rooms.belongsTo(this.MenuOrders, { foreignKey: 'room_id' })
-    this.Menu.belongsTo(this.MenuOrders, { foreignKey: 'menu_id' })
-    this.MenuOrders.hasMany(this.Rooms, { foreignKey: 'room_id' })
-    this.MenuOrders.hasMany(this.Menu, { foriegnKey: 'menu_id' })
+    //this.Rooms.belongsTo(this.MenuOrders, { foreignKey: 'room_id' })
+    //this.Menu.belongsTo(this.MenuOrders, { foreignKey: 'menu_id' })
+    //this.MenuOrders.hasMany(this.Rooms, { foreignKey: 'room_id' })
+    //this.MenuOrders.hasMany(this.Menu, { foriegnKey: 'menu_id' })
 
-    this.Rooms.belongsTo(this.RoomServices, { foreignKey: 'room_id' })
-    this.Task.belongsTo(this.RoomServices, { foreignKey: 'task_id' })
-    this.RoomServices.hasMany(this.Rooms, { foreignKey: 'room_id' })
-    this.RoomServices.hasMany(this.Task, { foreignKey: 'task_id' })
+    //this.Rooms.belongsTo(this.RoomServices, { foreignKey: 'room_id' })
+    //this.Task.belongsTo(this.RoomServices, { foreignKey: 'task_id' })
+    //this.RoomServices.hasMany(this.Rooms, { foreignKey: 'room_id' })
+    //this.RoomServices.hasMany(this.Task, { foreignKey: 'task_id' })
 
-    this.Guest.belongsTo(this.Reservations, { foreignKey: 'guest_id' })
-    this.Rooms.belongsTo(this.Reservations, { foreignKey: 'room_id' })
-    this.Reservations.hasMany(this.Guest, { foreignKey: 'guest_id' })
-    this.Reservations.hasMany(this.Rooms, { foreignKey: 'room_id' })
+    //this.Guest.belongsTo(this.Reservations, { foreignKey: 'guest_id' })
+    //this.Rooms.belongsTo(this.Reservations, { foreignKey: 'room_id' })
+    //this.Reservations.hasMany(this.Guest, { foreignKey: 'guest_id' })
+    //this.Reservations.hasMany(this.Rooms, { foreignKey: 'room_id' })
 
     // added functions to model
+    this.Staff.findAllAccessible = async (staffId) => {
+      const query = {
+        where: { id: staffId }
+      }
+      const result = await this.Staff.findAll(query)
+      return result
+    }
+
+    this.Guest.findAllAccessible = async (guestId) => {
+      const query = {
+        where: { id: guestId }
+      }
+      const result = await this.Guest.findAll(query)
+      return result
+    }
+
     this.Menu.findAllAccessible = async (isActive, menuId) => {
       const query = {
         where: { id: menuId }
@@ -50,11 +66,51 @@ class ModelManager {
       return result
     }
 
+    this.Category.findAllAccessible = async (categoryId) => {
+      const query = {
+        where: { id: categoryId }
+      }
+      const result = await this.Category.findAll(query)
+      return result
+    }
+
     this.MenuOrders.findAllAccessible = async (menuOrderId) => {
       const query = {
         where: { id: menuOrderId }
       }
       const result = await this.MenuOrders.findAll(query)
+      return result
+    }
+
+    this.Rooms.findAllAccessible = async (roomId) => {
+      const query = {
+        where: { id: roomId }
+      }
+      const result = await this.Rooms.findAll(query)
+      return result
+    }
+
+    this.RoomServices.findAllAccessible = async (roomServiceId) => {
+      const query = {
+        where: { id: roomServiceId }
+      }
+      const result = await this.RoomServices.findAll(query)
+      return result
+    }
+
+    this.Task.findAllAccessible = async (taskId) => {
+      const query = {
+        where: { id: taskId }
+      }
+      const result = await this.Task.findAll(query)
+      return result
+    }
+
+    this.Reservations.findAllAccessible = async (reservationId) => {
+      const query = {
+        where: { id: reservationId }
+      }
+      const result = await this.Reservations.findAll(query)
       return result
     }
 
